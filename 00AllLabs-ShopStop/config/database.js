a6deef20617157a1b92ac26874126731346cb941
Tatyana Milanova <tanyta78@gmail.com>
2018-06-13 15:55:57 +0300

    shop stop lab 2

--- 00AllLabs-ShopStop/config/database.js
+++ 00AllLabs-ShopStop/config/database.js
@@ -1,25 +1,34 @@
-let products = [];
-let count =1;
+const fs = require('fs');
+const path = require('path');
+const dbPath = path.join(__dirname, '/database.json');
 
-module.exports.products={};
-
-module.exports.products.getAll = ()=>{
-	return products;
-};
-
-module.exports.products.add = (product)=>{
-	product.id=count++;
-	products.push(product);
-};
-
-module.exports.findByName = (name)=>{
-	let product = null;
-
-	for (let p of products) {
-		if(p.name === name){
-			return p;
-		}
+function getProducts() {
+	if (fs.existsSync(dbPath) === false) {
+		fs.writeFileSync(dbPath, '[]');
+		return [];
 	}
 
-	return product;
+	let json = fs.readFileSync(dbPath).toString() || '[]';
+	let products=JSON.parse(json);
+	return products;
+}
+
+function saveProducts(products) {
+	let json = JSON.stringify(products);
+	fs.writeFileSync(dbPath, json);
+}
+
+module.exports.products = {};
+
+module.exports.products.getAll = getProducts;
+
+module.exports.products.add = (product) => {
+	let products = getProducts();
+	product.id = products.length + 1;
+	products.push(product);
+	saveProducts(products);
+};
+
+module.exports.products.findByName = (name) => {
+	return getProducts().filter(p=>p.name.toLowerCase().includes(name));
 };
